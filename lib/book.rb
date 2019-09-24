534d86ab8761b21b98e189a53c5a931f9459c6c5
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-24 20:15:54 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,8 +11,17 @@ class Book
   def author=(author)
     @author = author 
   end
+  
   def author
     @author
+  end
+   
+   def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
   end
  
 end
